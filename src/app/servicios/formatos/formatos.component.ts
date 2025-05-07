5b54eaf90e1594f87610a25ca3e39ee8d6150fbc
Sebastian rivera <sebasrh1@hotmail.com>
2025-05-07 13:36:27 -0500

    Archivospdf y nuevo componente

---
+++ src/app/servicios/formatos/formatos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formatos',
+  imports: [],
+  templateUrl: './formatos.component.html',
+  styleUrl: './formatos.component.css'
+})
+export class FormatosComponent {
+
+}
